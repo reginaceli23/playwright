4f41d2fc1290cc5b77d160cb5f89292fefc99c68
@reginaceli <mariaregina.c29@gmail.com>
2025-02-01 21:45:52 +0700

    rename testcase

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,14 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('testcase pertama', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('testcase kedua', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
